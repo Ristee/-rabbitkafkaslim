bf567d8a7c7f83f4e6a9ce9c318e9d242ad2eae7
ristee <rristee@gmail.com>
2022-03-07 23:20:17 +0500

    add slim/psr, add response by slim

--- api/public/index.php
+++ api/public/index.php
@@ -2,9 +2,34 @@
 
 declare(strict_types=1);
 
-header('Content-Type: application/json');
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
-echo json_encode([
-    'name' => 'App Api',
-    'version' => '1.0',
-]);
+require '../vendor/autoload.php';
+
+$config = [
+    'settings' => [
+        'addContentLengthHeader' => false,
+    ]
+];
+
+$app = \Slim\Factory\AppFactory::create();
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function(ServerRequestInterface $request, ResponseInterface $response) {
+    $data = [
+        'name' => 'App Api',
+        'version' => '1.0',
+    ];
+
+    $response = $response
+        ->withHeader('Content-Type', 'application/json');
+
+    $response->getBody()
+        ->write(json_encode($data));
+
+    return $response;
+});
+
+$app->run();
