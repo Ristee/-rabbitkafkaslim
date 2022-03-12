99846d16d5669534ddb55475a00dce967005a679
ristee <rristee@gmail.com>
2022-03-12 22:57:55 +0500

    add signup test

--- api/src/Model/User/Entity/User/User.php
+++ api/src/Model/User/Entity/User/User.php
@@ -4,31 +4,90 @@ declare(strict_types=1);
 
 namespace Api\Model\User\Entity\User;
 
+use DateTimeInterface;
+
 class User
 {
     private const STATUS_WAIT = 'wait';
     private const STATUS_ACTIVE = 'active';
 
     private UserId $id;
-    private \DateTimeInterface $created_at;
+    private DateTimeInterface $date;
     private Email $email;
     private string $passwordHash;
-    private string $confirmToken;
+    private ConfirmToken $confirmToken;
     private string $status;
 
+    /**
+     * @param UserId $id
+     * @param DateTimeInterface $created_at
+     * @param Email $email
+     * @param string $hash
+     * @param ConfirmToken $token
+     */
     public function __construct(
         UserId $id,
-        \DateTimeInterface $created_at,
+        DateTimeInterface $created_at,
         Email $email,
         string $hash,
-        string $token
+        ConfirmToken $token
     )
     {
         $this->id = $id;
-        $this->created_at = $created_at;
+        $this->date = $created_at;
         $this->email = $email;
         $this->passwordHash = $hash;
         $this->confirmToken = $token;
         $this->status = self::STATUS_WAIT;
+    }
+
+    public function isWait(): bool
+    {
+        return $this->status === self::STATUS_WAIT;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status === self::STATUS_ACTIVE;
+    }
+
+    /**
+     * @return UserId
+     */
+    public function getId(): UserId
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getDate(): DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return Email
+     */
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return ConfirmToken
+     */
+    public function getToken(): ConfirmToken
+    {
+        return $this->confirmToken;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHash(): string
+    {
+        return $this->passwordHash;
     }
 }
