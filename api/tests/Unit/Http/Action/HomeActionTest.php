a72cb9b7f90f433e3a832bcb2098a2c183ec821b
ristee <rristee@gmail.com>
2022-03-12 14:16:47 +0500

    add integrated test with phpunit

--- api/tests/Unit/Http/Action/HomeActionTest.php
+++ api/tests/Unit/Http/Action/HomeActionTest.php
@@ -1,9 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Api\Test\Unit\Http\Action;
 
 use Api\Http\Action\HomeAction;
+use DI\ContainerBuilder;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+use Slim\Factory\AppFactory;
 use Slim\Psr7\Factory\RequestFactory;
 use Slim\Psr7\Factory\ResponseFactory;
 
@@ -11,19 +19,54 @@ class HomeActionTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $action = new HomeAction();
-        $request = (new RequestFactory())->createRequest('GET', '/');
-        $response = (new ResponseFactory())->createResponse();
+        $request = $this->get('/');
 
-        $action->index($request, $response);
+        $response = $this->request($request);
 
         self::assertEquals(200, $response->getStatusCode());
-        self::assertJson($content = $response->getBody());
+
+        $content = (string)$response->getBody();
+        self::assertJson($content);
 
         $data = json_decode($content, true);
         self::assertEquals([
            'name' => 'App Api',
            'version' => '1.0',
         ], $data);
+    }
+
+    protected function app(): App
+    {
+        $containerBuilder = new ContainerBuilder();
+        // some settings
+        $container = $containerBuilder->build();
+
+        AppFactory::setContainer($container);
+        $app = AppFactory::create();
+
+        // middleware
+        $middleware = require __DIR__.'/../../../../routes/routes.php';
+        $middleware($app);
+
+        // routes
+        $routes = require __DIR__.'/../../../../config/middleware.php';
+        $routes($app);
+
+        return $app;
+    }
+
+    protected function method($method, $uri): RequestInterface
+    {
+        return (new RequestFactory())->createRequest($method, $uri);
+    }
+
+    protected function get($uri): RequestInterface
+    {
+        return $this->method('GET', $uri);
+    }
+
+    protected function request(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->app()->handle($request);
     }
 }
