c082335ca280acd60d2dbc57a0e96beffd7a4a05
ristee <rristee@gmail.com>
2022-03-12 14:32:56 +0500

    add webtestcase

---
+++ api/tests/Feature/WebTestCase.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Test\Feature;
+
+use DI\ContainerBuilder;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Factory\RequestFactory;
+
+class WebTestCase extends TestCase
+{
+    protected function app(): App
+    {
+        $containerBuilder = new ContainerBuilder();
+        // some settings
+        $container = $containerBuilder->build();
+
+        AppFactory::setContainer($container);
+        $app = AppFactory::create();
+
+        // middleware
+        $middleware = require __DIR__.'/../../routes/routes.php';
+        $middleware($app);
+
+        // routes
+        $routes = require __DIR__.'/../../config/middleware.php';
+        $routes($app);
+
+        return $app;
+    }
+
+    protected function method($method, $uri): RequestInterface
+    {
+        return (new RequestFactory())->createRequest($method, $uri);
+    }
+
+    protected function get($uri): RequestInterface
+    {
+        return $this->method('GET', $uri);
+    }
+
+    protected function request(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->app()->handle($request);
+    }
+}
--- api/tests/Unit/Http/Action/HomeActionTest.php
+++ api/tests/Unit/Http/Action/HomeActionTest.php
@@ -4,18 +4,9 @@ declare(strict_types=1);
 
 namespace Api\Test\Unit\Http\Action;
 
-use Api\Http\Action\HomeAction;
-use DI\ContainerBuilder;
-use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Slim\App;
-use Slim\Factory\AppFactory;
-use Slim\Psr7\Factory\RequestFactory;
-use Slim\Psr7\Factory\ResponseFactory;
+use Api\Test\Feature\WebTestCase;
 
-class HomeActionTest extends TestCase
+class HomeActionTest extends WebTestCase
 {
     public function testSuccess(): void
     {
@@ -33,40 +24,5 @@ class HomeActionTest extends TestCase
            'name' => 'App Api',
            'version' => '1.0',
         ], $data);
-    }
-
-    protected function app(): App
-    {
-        $containerBuilder = new ContainerBuilder();
-        // some settings
-        $container = $containerBuilder->build();
-
-        AppFactory::setContainer($container);
-        $app = AppFactory::create();
-
-        // middleware
-        $middleware = require __DIR__.'/../../../../routes/routes.php';
-        $middleware($app);
-
-        // routes
-        $routes = require __DIR__.'/../../../../config/middleware.php';
-        $routes($app);
-
-        return $app;
-    }
-
-    protected function method($method, $uri): RequestInterface
-    {
-        return (new RequestFactory())->createRequest($method, $uri);
-    }
-
-    protected function get($uri): RequestInterface
-    {
-        return $this->method('GET', $uri);
-    }
-
-    protected function request(ServerRequestInterface $request): ResponseInterface
-    {
-        return $this->app()->handle($request);
     }
 }
